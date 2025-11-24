f9a326f76fe803bca24ebbee924b2230456c7aab
Hannes Riebl <hriebl@posteo.de>
2025-11-24 15:19:54 +0100

    Convert relative to absolute paths

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import tomllib
+from pathlib import Path
 
 import geopandas as gpd
 
 
 def main():
+    directory = Path(__file__).parent
+
     try:
-        with open("config.toml", "rb") as f:
+        with open(directory / "config.toml", "rb") as f:
             config = tomllib.load(f)
 
         conversions = config.pop("convert", None)
         assert isinstance(conversions, list), "Only [[convert]] entries allowed"
         assert not config, "Only [[convert]] entries allowed"
     except Exception as e:
-        print(f"Invalid config.toml: {e}")
-        raise SystemExit(1)
+        raise RuntimeError(f"Invalid config.toml: {e}")
 
     for c in conversions:
         try:
@@ -22,6 +24,15 @@ def main():
 
             assert isinstance(_from, str), '[[convert]] entry needs from = "..."'
             assert isinstance(to, str), '[[convert]] entry needs to = "..."'
+
+            _from = Path(_from)
+            to = Path(to)
+
+            if not _from.is_absolute():
+                _from = (directory / _from).resolve()
+
+            if not to.is_absolute():
+                to = (directory / to).resolve()
 
             layer = c.get("layer", None)
 
